facb661347b6bbd9f85bc0d468b4c3aec6fa9c98
maddsua <maddsuadev@pm.me>
2025-02-25 14:56:33 +0100

    Create storage.go

---
+++ service/storage/storage.go
@@ -0,0 +1,41 @@
+package storage
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type LogEntry struct {
+	Time     time.Time
+	Level    Level
+	Message  string
+	Labels   Metadata
+	Metadata Metadata
+}
+
+type Level string
+
+func (lvl Level) String() string {
+	switch lvl {
+	case "log", "warn", "error", "debug", "info":
+		return string(lvl)
+	default:
+		return "error"
+	}
+}
+
+type Metadata map[string]string
+
+func (this *Metadata) Data() []byte {
+
+	data, err := json.Marshal(this)
+	if err != nil || this == nil {
+		return []byte("{}")
+	}
+
+	return data
+}
+
+func (this *Metadata) FromData(data []byte) error {
+	return json.Unmarshal(data, this)
+}
